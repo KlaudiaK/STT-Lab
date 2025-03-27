2129cd0d2bd1626b7a417e0261f5876bc4a62d4f
KlaudiaK <klaudia02460@gmail.com>
2025-03-27 21:04:27 +0100

    Cleanup application class

--- app/src/main/java/com/android/klaudiak/sttlab/STTLabApp.kt
+++ app/src/main/java/com/android/klaudiak/sttlab/STTLabApp.kt
@@ -1,15 +1,7 @@
 package com.android.klaudiak.sttlab
 
 import android.app.Application
-import com.android.klaudiak.audioplayer.AudioPlayerViewModel
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class STTLabApp : Application() {
-    lateinit var audioPlayerViewModel: AudioPlayerViewModel
-
-    override fun onCreate() {
-        super.onCreate()
-        audioPlayerViewModel = AudioPlayerViewModel(this)
-    }
-}
+class STTLabApp : Application()
